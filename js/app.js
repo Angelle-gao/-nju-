7d0d95e20321e1f2062cebdb4480759994880c36
g-herbaby <45869248+g-herbaby@users.noreply.github.com>
2020-07-30 10:26:54 +0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,30 @@
+(function($){
+"use strict";
+	$(function(){
+		$('#menu').slicknav(
+				{
+					label: '',
+					// prependTo:'',
+				}
+			);
+		});
+
+
+//if($("#googleMap")[0]) { 
+
+//	function initialize() {
+	
+//	  var mapProp = {
+//	    center:new google.maps.LatLng(37.331401462544335,-121.89474478205568),
+//	    zoom:12,
+//	    mapTypeId:google.maps.MapTypeId.ROADMAP
+//	  };
+	  
+//	  var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+//	} 
+//	google.maps.event.addDomListener(window, 'load', initialize);
+// }
+
+
+ console.log("Hey") ;
+})(jQuery)
---
+++ js/index.js
@@ -0,0 +1,18 @@
+//Inspired by a dribbble by Bluroon
+//https://dribbble.com/shots/1356864-Contact-Us
+
+
+//Capture the click event on an location
+$("#location-bar a").click(function(event){
+  event.preventDefault();
+  
+  var selectedMap = $(this).attr("href");
+  var selectedLocation = $(this).data('location');
+  
+    //Update #map bkimage with the image from the location
+    $('#map').css('background-image', 'url(' + selectedMap + ')');
+  
+    //update tooltip address
+    $('.selectedLocation').text(selectedLocation);
+
+});
